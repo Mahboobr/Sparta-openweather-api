d7060228e1c23ed7e078168b60e818b8a6b2630a
Tech-A28 <tech-a28@Tech-A28s-MacBook-Air.local>
2018-03-22 22:52:31 +0000

    created methods for tests

--- lib/services/weather_service.rb
+++ lib/services/weather_service.rb
@@ -7,7 +7,28 @@ class WeatherService
   base_uri 'http://api.openweathermap.org/data/2.5'
 
   def weather_response(city_name)
-    JSON.parse(self.class.get("/weather?q=#{city_name}&appid=5304ef4e7e25c61248f8f0ad8efb2078").body)
+    @weatherapi = JSON.parse(self.class.get("/weather?q=#{city_name}&appid=5304ef4e7e25c61248f8f0ad8efb2078").body)
+  end
+  def name_london
+    @weatherapi["name"]
+  end
+  def weather_array
+    @weatherapi["weather"]
+  end
+  def weather_main
+    @weatherapi["weather"][0]["main"]
+  end
+  def main_temp
+    @weatherapi["main"]["temp"]
+  end
+  def wind_speed
+    @weatherapi["wind"]["speed"]
+  end
+  def sys_hash
+    @weatherapi["sys"]
+  end
+  def sys_country
+    @weatherapi["sys"]["country"]
   end
 
 end
--- lib/weather.rb
+++ lib/weather.rb
@@ -2,14 +2,15 @@ require_relative "./services/weather_service"
 
 
 class WeatherAPI
-  include HTTParty
-
-  base_uri 'http://api.openweathermap.org/data/2.5'
+  # include HTTParty
+  #
+  # base_uri 'http://api.openweathermap.org/data/2.5'
   # base_uri 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8baef4fc56450ca58eae20c46f2336d2'
 
   def weather_service
     WeatherService.new
   end
+
 
 end
 # x = WeatherAPI.new
